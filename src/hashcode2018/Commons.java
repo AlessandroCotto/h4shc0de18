52c1c14c9ab11a67a01f344fc92d6e1c3e123cc2
Federico Ballarini <federico.ballarini.sc@gmail.com>
2018-02-28 16:31:13 +0100

    first commit to instanciate project

---
+++ src/hashcode2018/Commons.java
@@ -0,0 +1,18 @@
+package hashcode2018;
+
+/**
+ * Utility class
+ * 
+ * @author federicoballarini
+ *
+ */
+public class Commons {
+	
+	public static final void readFile(String filename) {
+		System.out.println("Here's where I read file: "+ filename);
+	}
+	
+	public static final void saveFile(String filename) {
+		System.out.println("Here's where I save file to: "+ filename);
+	}
+}
---
+++ src/hashcode2018/Engine.java
@@ -0,0 +1,18 @@
+package hashcode2018;
+
+/**
+ * main class 
+ * 
+ * @author federicoballarini
+ *
+ */
+public class Engine {
+	/**
+	 * @param args
+	 */
+	public static void main(String args[]) {
+		
+		System.out.println("Hello World!");
+		
+	}
+}
